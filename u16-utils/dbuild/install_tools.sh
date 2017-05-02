9715da2d14303a48e504a81a9fd62483dc1e2933
Kunfeng Chen <kunfengchen@live.com>
2017-05-01 19:13:38 -0700

    Added sudo

--- u16-utils/dbuild/install_tools.sh
+++ u16-utils/dbuild/install_tools.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-apt-get install -y cmake vim git tmux usbutils lsb-release
+apt-get install -y cmake vim git tmux usbutils lsb-release \
+    sudo
 
 # GUI related
 apt-get install -y xterm libgtk2.0-0 
