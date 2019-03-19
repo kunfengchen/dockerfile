1cc4d64bb113744c8600dc74b22ab5a4e6340c67
Kunfeng Chen <kunfengchen@live.com>
2019-03-18 21:57:35 -0700

    update

--- u16-tf20a/dbuild/install_python_dep.sh
+++ u16-tf20a/dbuild/install_python_dep.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-apt-get install -y python3.5 python3.5-dev
-apt-get install -y python3-pip
-pip3 install --upgrade pip
-
-# pip3 install jupyter
-
 pip3 --no-cache-dir install \
         ipykernel \
         jupyter \
