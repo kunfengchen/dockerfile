718dbd92dce85d68b199b5a96efa2ad256093496
Kunfeng Chen <kunfengchen@live.com>
2019-03-18 20:15:16 -0700

    update

--- u14-indigo-autoware/docker-run-osx-sysint.sh
+++ u14-indigo-autoware/docker-run-osx-sysint.sh
@@ -3,7 +3,7 @@
 ip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
 
 docker run -it --rm \
-   -p 4568:4567 \
+   -p 4567:4567 \
    --env="DISPLAY=$ip:0" \
    --volume /tmp/.X11-unix:/tmp/.X11-unix \
    --volume "$HOME/sharefolder:/sharefolder" \
